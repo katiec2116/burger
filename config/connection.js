8ff8d3ff00874994c53a9f52e5d4be9c940834ea
katiec <katiecampbell21@outlook.com>
2020-10-18 17:02:45 -0400

    edit orm file

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,5 @@
-var express = require("express");
-var exphbs = require("express-handlebars");
 var mysql = require("mysql");
-var app = express();
-var PORT = process.env.PORT || 8080;
 
-// Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-app.use(express.static("public"));
-
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -30,7 +18,4 @@ connection.connect(function(err) {
 });
 
 
-app.listen(PORT, function() {
-    // Log (server-side) when our server has started
-    console.log("Server listening on: http://localhost:" + PORT);
-  });
+module.exports = connection;
---
+++ public/assets/js/burger.js
@@ -0,0 +1,61 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+    $(".devour").on("click", function(event) {
+      var id = $(this).data("id");
+      var devour = $(this).data("status");
+      console.log(devour)
+      var newState = {
+        devoured: devour
+      };
+      console.log(newState)
+  
+      // Send the PUT request.
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: newState
+      }).then(
+        function() {
+          console.log("changed sleep to", devour);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".create-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+  
+      var newBurger = {
+        burger_name: $("#burg").val().trim()
+      };
+  
+      // Send the POST request.
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("created new burger");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+  
+    $(".delete-burger").on("click", function(event) {
+      var id = $(this).data("id");
+  
+      // Send the PUT request.
+      $.ajax("/api/burgers/" + id, {
+        type: "DELETE",
+      }).then(
+        function() {
+          console.log("Deleted burger", id);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  });
