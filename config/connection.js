0cb74fe6001b66c456ec51639d74393c7ef212af
katiec <katiecampbell21@outlook.com>
2020-10-19 15:15:15 -0400

    edit connection

--- config/connection.js
+++ config/connection.js
@@ -7,21 +7,14 @@ if(process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else{
   connection = mysql.createConnection({
-  host: "vrk7xcrab1wsx4r1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  host: "localhost",
   port: 3306,
-  user: "ph72yamab7c6har0",
-  password: "wnpxni1hjx7n2c9p",
-  database: "uakgvxjfzu73m32k"
+  user: "root",
+  password: "rootroot",
+  database: "burgers_db"
   });
 };
 
-// = mysql.createConnection({
-//   host: "localhost",
-//   port: 3306,
-//   user: "root",
-//   password: "rootroot",
-//   database: "burgers_db"
-// });
 
 connection.connect(function(err) {
   if (err) {
